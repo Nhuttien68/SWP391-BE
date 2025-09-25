8102704787a77177b7433fd208c625a7c26a3b0a
Tien <voluongnhuttien@gmail.com>
2025-09-26 00:01:51 +0700

    Register for user

---
+++ EVMarketPlace.API/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using EVMarketPlace.Repositories.RequestDTO;
+using EVMarketPlace.Repositories.ResponseDTO;
+using EVMarketPlace.Services.Interfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EVMarketPlace.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private IUserService _userService;
+
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+        /// <summary>
+        /// API dùng để đăng ký tài khoản người dùng mới.
+        /// </summary>
+        [HttpPost("register")]
+        public async Task<CreateAccountRespone> Create(CreateAccountRequest request)
+        {
+            return await _userService.CreateAccount(request);
+        }
+    }
+}
--- EVMarketPlace.API/Program.cs
+++ EVMarketPlace.API/Program.cs
@@ -1,3 +1,7 @@
+using EVMarketPlace.Repositories.Repository;
+using EVMarketPlace.Services.Implements;
+using EVMarketPlace.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<UserRepository>();  
 
 var app = builder.Build();
 
