8a4dd33bbbbcb165fb5231871d41e998a72ad68d
tandat1408 <tandatofficial3@gmail.com>
2025-09-28 23:54:53 +0700

    Revert: không custom Entity; sẽ dùng DTO

--- EVMarketPlace.Repositories/Entity/Post.cs
+++ EVMarketPlace.Repositories/Entity/Post.cs
@@ -1,28 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations.Schema;   // [NotMapped]
-using System.Text.Json.Serialization;                // [JsonIgnore]
 
-namespace EVMarketPlace.Repositories.Entity
+namespace EVMarketPlace.Repositories.Entity;
+
+public partial class Post
 {
-    public partial class Post
-    {
-        public Guid PostId { get; set; }
-        public Guid UserId { get; set; }
-        public string Type { get; set; } = null!;
-        public string Title { get; set; } = null!;
-        public string? Description { get; set; }
-        public decimal Price { get; set; }
-        public bool IsActive { get; set; }
-        public DateTime CreatedAt { get; set; }
+    public Guid PostId { get; set; }
 
-        // Navigation props: tạm bỏ qua để tránh EF map quan hệ
-        [NotMapped, JsonIgnore] public virtual ICollection<Auction>? Auctions { get; set; }
-        [NotMapped, JsonIgnore] public virtual ICollection<Battery>? Batteries { get; set; }
-        [NotMapped, JsonIgnore] public virtual ICollection<CartItem>? CartItems { get; set; }
-        [NotMapped, JsonIgnore] public virtual ICollection<Favorite>? Favorites { get; set; }
-        [NotMapped, JsonIgnore] public virtual ICollection<Transaction>? Transactions { get; set; }
-        [NotMapped, JsonIgnore] public virtual User? User { get; set; }
-        [NotMapped, JsonIgnore] public virtual ICollection<Vehicle>? Vehicles { get; set; }
-    }
+    public Guid UserId { get; set; }
+
+    public string Type { get; set; } = null!;
+
+    public string Title { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public bool IsActive { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual ICollection<Auction> Auctions { get; set; } = new List<Auction>();
+
+    public virtual ICollection<Battery> Batteries { get; set; } = new List<Battery>();
+
+    public virtual ICollection<CartItem> CartItems { get; set; } = new List<CartItem>();
+
+    public virtual ICollection<Favorite> Favorites { get; set; } = new List<Favorite>();
+
+    public virtual ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+    public virtual User User { get; set; } = null!;
+
+    public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
 }
